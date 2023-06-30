d29059e31a8024e87304c545770827db3047547e
master <master@SMLAB108PC23_23>
2023-06-29 21:09:27 -0500

    Agregar archivos de proyecto.

---
+++ S6crodriguez/S6crodriguez/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace S6crodriguez
+{
+    public partial class MainPage : ContentPage
+    {
+        private string URL = "http://192.168.10.45/ws_uisrael/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<estudiante> post;
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Obtener();
+        }
+
+        public async void Obtener()
+        {
+            var contenido = await client.GetStringAsync(URL);
+            List<estudiante> datosPost = JsonConvert.DeserializeObject<List<estudiante>>(contenido);
+            post = new ObservableCollection<estudiante>(datosPost);
+            listaEstudiantes.ItemsSource = post;
+        }
+        private async void btnConsultar_Clicked(object sender, EventArgs e)
+        {
+           
+
+        }
+    }
+}
---
+++ S6crodriguez/S6crodriguez/estudiante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S6crodriguez
+{
+    public class estudiante
+    {
+        public int codigo { set; get; }
+        public string nombre { set; get; }
+        public string apellido { set; get; }
+        public int edad { set; get; }
+
+    }
+}
